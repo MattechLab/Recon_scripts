f15e383c3997618493c0d6b1afb33b0074975732
“Evelyn92” <Evelynjia92@gmail.com>
2025-12-25 13:26:35 +0100

    add fixed sop and modify the twix pmu functions

--- func/twix_pulseq_process_yj/archive/S1_resolve_twix_ext_archive.m
+++ func/twix_pulseq_process_yj/archive/S1_resolve_twix_ext_archive.m
@@ -3,6 +3,7 @@
 % information as "report" for Dataset "MREyeTrack" acquired on Oct. 14 2024
 %%
 clc; clear all;
+<<<<<<< Updated upstream:func/twix_pulseq_process_yj/archive/S1_resolve_twix_ext_archive.m
 subject_num = 1;
 meas_name = ['meas', '_MID00332_FID214628_BEAT_LIBREon_eye_(23_09_24)_sc_trigger'];
 raw_data = ['/Users/cag/Documents/Dataset/datasets/250127_acquisition/', meas_name,'.dat'];
@@ -47,10 +48,59 @@ stop_time_ms = (rawTimestamp(end)-rawTimestamp(1)) * 2.5;
 rawTime_ms = linspace(0, stop_time_ms, length(rawTimestamp));
 ext1 = double(rawEXTData(:,1));
 ext2 = double(rawEXTData(:,2));%remember to convert the data type, otherwise something wrong with rawTimestamp.
+=======
+% Select raw data file
+baseFolder = '/mnt/filer01/MatTechLab/yiwei.jia/';
+[measFilename, rawDir] = uigetfile('*.dat', 'Select main raw data file', baseFolder);
+
+if measFilename == 0
+    error('measurement data file selection was cancelled');
+end
+
+measureFile = fullfile(rawDir, measFilename);
+%%
+twix = mapVBVD_JB(measureFile);
+
+twix_img = twix{end};
+PMU = twix_img.PMUdata;
+
+sum(sum(PMU.EXT))
+sum(sum(PMU.raw.EXT.data))
+measSplit = split(measFilename, '.');
+twix_name = ['twix_', measSplit{1}];
+twixFolder = [rawDir, '/twix/'];
+if ~isfolder(twixFolder)
+    mkdir(twixFolder);
+end
+
+twix_path =  [twixFolder, twix_name,'.mat'];
+disp(['Saving twix into', twix_path]);
+save(twix_path, 'twix');
+
+%
+
+
+[twixFilename, twixDir] = uigetfile('*.mat', 'Select twix data file', baseFolder);
+
+twix = load(fullfile(twixDir, twixFilename));
+
+%
+twix_image2 = twix.twix{1,end};
+pmuEXT = twix_image2.PMUdata.EXT;
+pmuTimestamp = twix_image2.image.timestamp;
+
+%%
+stop_time_ms = (pmuTimestamp(end)-pmuTimestamp(1)) * 2.5;
+disp(['The duration of pmu timestamps: ',num2str(stop_time_ms)]);
+time_ms = linspace(0, stop_time_ms, length(pmuTimestamp));
+ext1 = double(pmuEXT(1,:));
+ext2 = double(pmuEXT(2,:));%remember to convert the data type, otherwise something wrong with rawTimestamp.
+>>>>>>> Stashed changes:func/twix_process_yj/S1_resolve_twix_ext.m
 %
 raw_pmu_ext = array2table([rawTimestamp'; rawTime_ms(:)' ; ext1(:)'; ext2(:)']', 'VariableNames', ...
     {'PMU_timestamp', 'PMU_time_ms', 'EXT1', 'EXT2'});
 
+<<<<<<< Updated upstream:func/twix_pulseq_process_yj/archive/S1_resolve_twix_ext_archive.m
 
 %%
 
@@ -75,30 +125,45 @@ raw_pmu_mark((raw_pmu_ext.EXT1 == 0) & (raw_pmu_ext.EXT2 == 0),:)=[];
 
 diff_ms = diff(raw_pmu_mark.PMU_time_ms);
 diff_stamp = diff(raw_pmu_mark.PMU_timestamp);
+=======
+pmu_mark_table = pmu_ext_table;
+pmu_mark_table((pmu_ext_table.EXT1 == 0) & (pmu_ext_table.EXT2 == 0),:)=[];
+%%
+figure;
+plot(time_ms, pmu_ext_table.EXT1, time_ms, pmu_ext_table.EXT2)
+xlabel('time (ms)')
+ylabel('pulse amplitude')
+
+
+%%
+
+diff_ms = diff(pmu_mark_table.PMU_time_ms);
+diff_stamp = diff(pmu_mark_table.PMU_timestamp);
+>>>>>>> Stashed changes:func/twix_process_yj/S1_resolve_twix_ext.m
 disp('The time diff between start of EXT and the first measurement of MRI:')
-diff_start_mri = (raw_pmu_ext.PMU_timestamp(1) - mriTimeStamp(1))*2.5
+diff_start_mri = (pmu_ext_table.PMU_timestamp(1) - pmuTimestamp(1))*2.5
 disp('The time diff between start of EXT and the first mark of EXT:')
-diff_start_mark = (raw_pmu_ext.PMU_timestamp(1) - raw_pmu_mark.PMU_timestamp(1))*2.5
+diff_start_mark = (pmu_ext_table.PMU_timestamp(1) - pmu_mark_table.PMU_timestamp(1))*2.5
 disp('the first mark of EXT - the first mri measure:')
 disp(num2str(diff_start_mri - diff_start_mark))
 
-%% Generate a cell report
+% Generate a cell report
 report={};
-report{1,1}='pmu_timestp_start'; report{1,2}=raw_pmu_ext.PMU_timestamp(1);
-report{2,1}='pmu_timestp_end'; report{2,2}=raw_pmu_ext.PMU_timestamp(end);
-report{3,1}='pmu_mark_start'; report{3,2}=raw_pmu_mark.PMU_timestamp(1);
-report{4,1}='pmu_mark_end'; report{4,2}=raw_pmu_mark.PMU_timestamp(end);
-report{5,1}='raw_pmu_ext'; report{5,2}=raw_pmu_ext;
-report{6,1}='raw_pmu_mark'; report{6,2}=raw_pmu_mark;
+report{1,1}='pmu_timestp_start'; report{1,2}=pmu_ext_table.PMU_timestamp(1);
+report{2,1}='pmu_timestp_end'; report{2,2}=pmu_ext_table.PMU_timestamp(end);
+report{3,1}='pmu_mark_start'; report{3,2}=pmu_mark_table.PMU_timestamp(1);
+report{4,1}='pmu_mark_end'; report{4,2}=pmu_mark_table.PMU_timestamp(end);
+report{5,1}='pmu_ext_table'; report{5,2}=pmu_ext_table;
+report{6,1}='pmu_mark_table'; report{6,2}=pmu_mark_table;
 
-report{7,1}='mri_timestp_start'; report{7,2}=mriTimeStamp(1);
-report{8,1}='mri_timestp_end'; report{8,2}=mriTimeStamp(end);
-report{9,1}='mriTimeStamp'; report{9,2}=mriTimeStamp;
+report{7,1}='mri_timestp_start'; report{7,2}=pmuTimestamp(1);
+report{8,1}='mri_timestp_end'; report{8,2}=pmuTimestamp(end);
+report{9,1}='pmuTimestamp'; report{9,2}=pmuTimestamp;
 disp('The report cell is generated!')
 %%
 
-reportPath = ['/home/debi/jaime/acquisitions/MREyeTrack/Twix/' ...
-        'report_subj', num2str(subject_num),'.mat'];
+reportPath = [twixDir ...
+        'report_', measSplit{1},'.mat'];
 save(reportPath, 'report');
 disp('report has been saved here:')
 disp(reportPath)
---
+++ sop_fixed/coilsense_script.m
@@ -0,0 +1,168 @@
+%% Init
+clc; clearvars;
+
+baseFolder = '/mnt/filer01/MatTechLab/yiwei.jia/';
+%% === User Input Section ===
+% Open file dialogs for sequence and raw data files
+
+% Select sequence file
+[seqName, seqFolder] = uigetfile('*.seq', 'Select prescan sequence file', baseFolder);
+if seqName == 0
+    error('Sequence file selection was cancelled');
+end
+
+% Select raw data file
+[bodyCoilFilename, rawDir] = uigetfile('*.dat', 'Select bc data file', baseFolder);
+if bodyCoilFilename == 0
+    error('bodyCoil data file selection was cancelled');
+end
+% Select raw data file
+[arrayCoilFilename, rawDir] = uigetfile('*.dat', 'Select hc data file', baseFolder);
+
+if arrayCoilFilename == 0
+    error('arrayCoil data file selection was cancelled');
+end
+
+% Create cell array with the selected sequence file
+seqName_list = {seqName};
+%% === Add paths ===
+addpath(genpath('/home/debi/jaime/repos/MR-EyeTrack/recon'));
+addpath(genpath('/home/debi/MatTechLab/monalisa'));
+addpath(genpath('/home/debi/yiwei/forclone/pulseq'));
+addpath(genpath('/home/debi/yiwei/forclone/mapVBVD'))
+%% Initialize the directories and acquire the Coil
+
+% Parameters
+saveflag = 1;
+
+% Full path to the measurement file
+bodyCoilFile = fullfile(rawDir, bodyCoilFilename);
+arrayCoilFile = fullfile(rawDir, arrayCoilFilename);
+% Recon directory (same level as rawDir)
+[parentDir, ~] = fileparts(rawDir);
+parts = strsplit(parentDir, filesep);
+parts(strcmp(parts,'mreye_dataset')) = {'recon_results'}; %replace the folder name as yiwei's convention
+parentDir = fullfile(filesep, parts{:});
+%
+token = regexp(arrayCoilFile, 'MID\d+', 'match');
+MID = token{1}; %extract MID
+reconDir = fullfile(parentDir, [MID '_recon_C']);
+
+
+%% Load and Configure Data
+% Read data using the library's `createRawDataReader` function
+% This readers makes the usage of Siemens and ISMRMRD files equivalent for
+% the library
+% Sequence file (already selected by user)
+seqFile = fullfile(seqFolder, seqName);
+seqParams = extract_seq_params(seqFile);
+
+
+bodyCoilreader = createRawDataReader(bodyCoilFile, true);
+if isfield(seqParams, 'nshot')
+    bodyCoilreader.acquisitionParams.nShot = seqParams.nshot;
+end
+if isfield(seqParams, 'nseg')
+    bodyCoilreader.acquisitionParams.nSeg = seqParams.nseg;
+end
+
+bodyCoilreader.acquisitionParams.nShot_off = 14;
+bodyCoilreader.acquisitionParams.traj_type = 'pulseq';
+bodyCoilreader.acquisitionParams.pulseqTrajFile_name = strcat(seqFolder, seqName);
+ % check if the hash from pulseq sequence and from twix match each other
+isMatch = check_hash(bodyCoilFile,bodyCoilreader.acquisitionParams.pulseqTrajFile_name);
+
+
+arrayCoilReader = createRawDataReader(arrayCoilFile, true);
+if isfield(seqParams, 'nshot')
+    arrayCoilReader.acquisitionParams.nShot = seqParams.nshot;
+end
+if isfield(seqParams, 'nseg')
+    arrayCoilReader.acquisitionParams.nSeg = seqParams.nseg;
+end
+arrayCoilReader.acquisitionParams.nShot_off = 14;
+arrayCoilReader.acquisitionParams.traj_type = 'pulseq';
+arrayCoilReader.acquisitionParams.pulseqTrajFile_name = strcat(seqFolder, seqName);
+ % check if the hash from pulseq sequence and from twix match each other
+isMatch = check_hash(arrayCoilFile,arrayCoilReader.acquisitionParams.pulseqTrajFile_name);
+
+% Ensure consistency in number o1f shot-off points
+nShotOff = arrayCoilReader.acquisitionParams.nShot_off;
+
+
+%% Parameters
+dK_u = [1, 1, 1] ./ arrayCoilReader.acquisitionParams.FoV;   % Cartesian grid spacing
+N_u = [48, 48, 48];             % Adjust this value as needed
+% Compute Trajectory and Volume Elements
+[y_body, t, ve] = bmCoilSense_nonCart_data(bodyCoilreader, N_u);
+y_surface = bmCoilSense_nonCart_data(arrayCoilReader, N_u);
+
+% Compute the gridding matrices (subscript is a reminder of the result)
+% Gn is from uniform to Non-uniform
+% Gu is from non-uniform to Uniform
+% Gut is Gu transposed
+[Gn, Gu, Gut] = bmTraj2SparseMat(t, ve, N_u, dK_u);
+%% Create Mask
+% This line below just does not work on mac
+mask = bmCoilSense_nonCart_mask_automatic(y_body, Gn, false);
+
+% % Box excluding coordinates
+% x_min = 1;  x_max = 40;
+% y_min = 9;  y_max = 41;
+% z_min = 4;  z_max = 48;
+% 
+% % Two thresholds
+% th_RMS = 14;  th_MIP = 10; 
+% close_size = [];  open_size  = []; 
+% m = bmCoilSense_nonCart_mask( y_body, Gn, ...
+%                                 x_min, x_max, ...
+%                                 y_min, y_max, ...
+%                                 z_min, z_max, ...
+%                                 th_RMS, th_MIP, ...
+%                                 close_size, ...
+%                                 open_size, ...
+%                                 true);
+
+%% Estimate Coil Sensitivity
+% Reference coil sensitivity using the body coils. This is used as 
+% a reference to estiamte the sensitivity of each head coil
+close all;
+[y_ref, C_ref] = bmCoilSense_nonCart_ref(y_body, Gn, mask, []);
+
+% Head coil sensitivity estimate using body coil reference
+C_array_prime = bmCoilSense_nonCart_primary(y_surface, y_ref, C_ref, Gn, ve, mask);
+% Refine the sensitivity estimate with optimization
+nIter = 5;
+[C1, x] = bmCoilSense_nonCart_secondary(y_surface, C_array_prime, y_ref, ...
+                                       C_ref, Gn, Gu, Gut, ve, nIter, false);
+
+% Display Results
+bmImage(C1);
+%% 
+check_orient_xrms(baseFolder);
+%%
+C = C1;
+for iCh = 1:size(C1,4)
+    C(:,:,:,iCh) = flip(permute(C1(:,:,:,iCh), [2 1 3]),1);
+    % C(:,:,:,iCh) = flip(permute(C(:,:,:,iCh), [2 1 3]),2);
+end
+bmImage(C)
+%% Save C into the folder
+
+saveCDir = [reconDir, '/C/'];
+
+CfileName = 'C.mat';
+
+% Create the folder if it doesn't exist
+if ~exist(saveCDir, 'dir')
+    mkdir(saveCDir);
+end
+
+% Full path to  C file
+CfilePath = fullfile(saveCDir, CfileName);
+
+% Save the matrix C to the .mat file
+save(CfilePath, 'C');
+disp('Coil sensitivity C has been saved here:')
+disp(CfilePath)
+
